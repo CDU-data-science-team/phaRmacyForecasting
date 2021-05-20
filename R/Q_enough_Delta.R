65f7c79367ba0ed2ae2c35271577455e10cc00bc
Chris Beeley <chris.beeley@gmail.com>
2021-05-20 09:19:46 +0100

    Whitespace

--- R/Q_enough_Delta.R
+++ R/Q_enough_Delta.R
@@ -4,20 +4,21 @@ Q_enough_Delta <- function(){
   # function used when Q_i has been reduced to meet storage condition
   
   epsilon <- 0.01
-  flag1 = 0
-  P_Q_insuff <- c(rep(1,Forecast_length)) # initialise as insufficient
-  Prob_y <-c(rep(0,Forecast_length)) # initialise to zero
-  term_y <- c(rep(0,Forecast_length))  
+  flag1 <- 0
+  P_Q_insuff <- c(rep(1, Forecast_length)) # initialise as insufficient
+  Prob_y <-c(rep(0, Forecast_length)) # initialise to zero
+  term_y <- c(rep(0, Forecast_length))  
   
   Q_out <- sum(Outstanding_orders$Ord_quant)
   
   # now set probabilities for next delivery arriving at time y CHECK DISCRETISATION AGAINST FORECAST
   # and Q_i being insufficient if that is the case
   
-  for (y in seq(1,Forecast_length,1)){    # all this as per Q_enough_Q
+  for (y in seq(1, Forecast_length, 1)){    # all this as per Q_enough_Q
     
     Prob_y[y] <- lead_time_dis$cdf(y - Delta_i)- lead_time_dis$cdf(y - Delta_i - 1)
-    P_Q_insuff[y] <- 1- pwlcdf(Forecast_quantiles, q_vals, num_q_vals, y, inv_i + Q_out + Q_i - min_stock)
+    P_Q_insuff[y] <- 1 - pwlcdf(
+      Forecast_quantiles, q_vals, num_q_vals, y, inv_i + Q_out + Q_i - min_stock)
   }
   
   term_y <- Prob_y * P_Q_insuff  
