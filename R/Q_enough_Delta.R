21d685a390dacf6445b7333b181fb2751d60a456
Chris Beeley <chris.beeley@gmail.com>
2021-05-20 09:18:00 +0100

    Whitespace

--- R/Q_enough_Delta.R
+++ R/Q_enough_Delta.R
@@ -16,8 +16,8 @@ Q_enough_Delta <- function(){
   
   for (y in seq(1,Forecast_length,1)){    # all this as per Q_enough_Q
     
-    Prob_y[y] <- lead_time_dis$cdf(y-Delta_i)- lead_time_dis$cdf(y-Delta_i-1)
-    P_Q_insuff[y] <- 1- pwlcdf(Forecast_quantiles,q_vals, num_q_vals,y,inv_i+Q_out+Q_i-min_stock)
+    Prob_y[y] <- lead_time_dis$cdf(y - Delta_i)- lead_time_dis$cdf(y - Delta_i - 1)
+    P_Q_insuff[y] <- 1- pwlcdf(Forecast_quantiles, q_vals, num_q_vals, y, inv_i + Q_out + Q_i - min_stock)
   }
   
   term_y <- Prob_y * P_Q_insuff  
@@ -26,7 +26,7 @@ Q_enough_Delta <- function(){
   
   if (phi > p_min) { # Q_i insufficient given current value of Delta_i
     
-    sc <-  (p_min / phi) * (1-epsilon) # get reduction required in phi
+    sc <-  (p_min / phi) * (1 - epsilon) # get reduction required in phi
     y_peak <- which.max(term_y) # find biggest term in phi
     
     P_target <- Prob_y[y_peak] * sc # find target probability for next delivery being ordered at y
@@ -35,23 +35,25 @@ Q_enough_Delta <- function(){
     test <- Prob_y[y_hat]
     
     # effectively we are bringing the order of the next forward until we get 
-    # desired reduction in the (currently) biggest constribution to the prob of Q_i being insufficient 
+    # desired reduction in the (currently) biggest contribution to the prob of Q_i being insufficient 
     
-    while(test>P_target){   
+    while(test > P_target){   
       
       y_hat <- y_hat + 1  
       test <- Prob_y[y_hat]
     }
     
-    Delta_i <- Delta_i -(y_hat-y_peak)
+    Delta_i <- Delta_i -(y_hat - y_peak)
     
   } else {
     
     flag1 = 1        
   }
-  if (Delta_i < 1) {print("CANNOT BE SOLVED WITH CURRENT INPUTS - REVISE STORAGE CONSTRAINT, 
-                          EMERGENCY STOCK OR TOLERANCES")} 
-  res <- c(Delta_i,flag1)
+  if (Delta_i < 1) {
+    print("CANNOT BE SOLVED WITH CURRENT INPUTS - REVISE STORAGE CONSTRAINT, 
+                          EMERGENCY STOCK OR TOLERANCES")
+  } 
+  res <- c(Delta_i, flag1)
   
   return(res)
 }
