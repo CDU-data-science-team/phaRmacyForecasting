b8dd5b40b13a21342d189c5699458a7cc4f52a62
Chris Beeley <chris.beeley@gmail.com>
2022-01-04 11:20:25 +0000

    Update inventory function

--- tests/testthat/test-inventory_reorder.R
+++ tests/testthat/test-inventory_reorder.R
@@ -1,5 +1,11 @@
 test_that("multiplication works", {
   
+  # If you're looking at the most recent data then 
+  # site 100 (stores) uses the suppliers AAH, MAW, PHOX and 
+  # sites 240, 242 & 958 use STO. 
+  # If you're looking at the very early data (2011) 
+  # then only site 240 existed at that point so try AAH & UNICH
+  
   reorder_frame <- inventory_reorder(site = 240, supplier = "STO")
   
   expect_gt(nrow(reorder_frame), 0)
